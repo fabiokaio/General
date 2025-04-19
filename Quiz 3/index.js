78b5a1f6e5c8a9b30afc4adce0840070657195ff
fabiokaio <fabiokaio501@gmail.com>
2025-04-19 13:14:45 +0000

    Criando Quiz semelhante ao anterior (sem IA)

---
+++ Quiz 3/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded')
+
+const pergunta = document.querySelector('.pergunta')
+const alterna = document.getElementsByClassName('alterna')
+const brr_progress = document.querySelector9('.linha')
